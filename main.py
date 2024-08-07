2175375810a328c0f7f9e05aeaac44323441acb3
Alex Ortiz <vlxrtz@gmail.com>
2024-08-06 22:02:38 -0400

    added json export and subreddit list, and fixed json parse/filter

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import time
 import httpx
-import pandas as pd
+import json
+from constants import subreddits
 
 
 def grabber():
-    # Retrieval point for any subreddit
-    base_url = "https://www.reddit.com"
-    endpoint = "/r/stocks"
-    category = "/hot"
+    # Capped at last 500 posts per sub for now
+    # Anything past 3 times per 100, the 4th is blank for some reason???
+    # Should probably make scheduler
 
-    url = base_url + endpoint + category + ".json"
-    after_post_id = None
+    for sub in subreddits:
+        print(f"Fetching ${sub} \n")
+        grab_sub(sub, 100, 4)
 
+
+def grab_sub(sub, posts, rate):
+    # Data
     dataset = []
 
-    # Capped at last 500 posts for now
-    # Should probably make scheduler
-    for _ in range(5):
-        params = {"limit": 100, "t": "year", "after": after_post_id}  # time units
+    # Grab chunk past ID
+    after_post_id = None
+
+    # Retrieval point for any subreddit
+    category = "hot"
+    url = f"https://www.reddit.com/r/{sub}/{category}.json"
+
+    for _ in range(rate):
+        params = {"limit": posts, "t": "year", "after": after_post_id}  # time units
         response = httpx.get(url, params=params)
 
-        print(f'fetching"{response.url}"...')
+        print(f'Fetching chunk {_} "{response.url}"... \n')
+
         if response.status_code != 200:
             raise Exception("Failed to fetch!")
 
         json_data = response.json()
 
-        dataset.extend([rec["data"] for rec in json_data["data"]["children"]])
+        parsed = [rec["data"] for rec in json_data["data"]["children"]]
+        keys = ["title", "url"]
+        filtered = [{key: item[key] for key in keys} for item in parsed]
 
+        dataset.extend(filtered)
+
+        # Set ID to grab next chunk
         after_post_id = json_data["data"]["after"]
         time.sleep(0.5)
 
-    df = pd.DataFrame(dataset)
-    df.to_csv("export/reddit_python.csv", index=True)
+    with open(f"export/{sub}.json", "w", encoding="utf-8") as f:
+        json.dump(dataset, f, indent=4, ensure_ascii=False)
 
 
 def main():
